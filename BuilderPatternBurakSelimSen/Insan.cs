8a127f42c7fb6a60de2e3c7ebed4013128c221e9
Yunus BULDUK <y_bulduk@aof.anadolu.edu.tr>
2018-12-04 14:36:44 +0300

    Add project files.

---
+++ BuilderPatternBurakSelimSen/Insan.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternBurakSelimSen
+{
+    public class Insan
+    {
+        public string Ad { get; set; }
+
+        public int Yas { get; set; }
+
+        public bool Cinsiyet { get; set; }
+
+        public string Memleket { get; set; }
+    }
+
+
+    public interface IInsanOzelik
+    {
+        Insan Olustur();
+        IInsanOzelik SetAd(string Ad);
+        IInsanOzelik SetYas(int Yas);
+        IInsanOzelik SetCinsiyet(bool Cinsiyet);
+        IInsanOzelik SetMemleket(string Memleket);
+
+
+    }
+
+    public class InsanFactory : IInsanOzelik
+    {
+        Insan _insan;
+
+        public InsanFactory(Insan insan)
+        {
+            _insan = insan;
+        }
+
+        public Insan Olustur()
+        {
+            return this._insan;
+        }
+
+        public IInsanOzelik SetAd(string Ad)
+        {
+            _insan.Ad = Ad;
+            return this;
+        }
+
+        public IInsanOzelik SetCinsiyet(bool Cinsiyet)
+        {
+            _insan.Cinsiyet = Cinsiyet;
+            return this;
+        }
+
+        public IInsanOzelik SetMemleket(string Memleket)
+        {
+            _insan.Memleket = Memleket;
+            return this;
+        }
+
+        public IInsanOzelik SetYas(int Yas)
+        {
+            _insan.Yas = Yas;
+            return this;
+        }
+    }
+
+    public static class InsanInit
+    {
+        public static InsanFactory Init()
+        {
+            return new InsanFactory(new Insan);
+        }
+
+    }
+}
---
+++ BuilderPatternBurakSelimSen/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternBurakSelimSen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero moonsta = FluentHeroFactory.Init()
+                .GiveAColor("Blue")
+                .GiveANickName("Monstar")
+                .SetForceTo(50)
+                .TakeHero();
+
+            Insan insan = InsanInit.Init()
+                .SetAd("Mehmet")
+                .SetCinsiyet(true)
+                .SetMemleket("Konya")
+                .SetYas(29)
+                .Olustur();
+        }
+    }
+
+    class Hero
+    {
+        public string NickName { get; set; }
+        public string Color { get; set; }
+        public byte[] Gravatar { get; set; }
+        public int InitialForce { get; set; }
+
+    }
+
+    interface IHeroFactory
+    {
+        Hero TakeHero();
+        IHeroFactory SetForceTo(int ForceValue);
+        IHeroFactory GiveANickName(string NickName);
+        IHeroFactory GiveAGravatar(byte[] GravatarPath);
+        IHeroFactory GiveAColor(string Color);
+
+    }
+
+    class HeroFactory : IHeroFactory
+    {
+        private Hero _hero;
+        public HeroFactory(Hero hero)
+        {
+            _hero = hero;
+        }
+        public IHeroFactory GiveAColor(string Color)
+        {
+            _hero.Color = Color;
+            return this;
+        }
+
+        public IHeroFactory GiveAGravatar(byte[] GravatarPath)
+        {
+            _hero.Gravatar = GravatarPath;
+            return this;
+        }
+
+        public IHeroFactory GiveANickName(string NickName)
+        {
+            _hero.NickName = NickName;
+            return this;
+        }
+
+        public IHeroFactory SetForceTo(int ForceValue)
+        {
+            _hero.InitialForce = ForceValue;
+            return this;
+        }
+
+        public Hero TakeHero()
+        {
+            return this._hero;
+        }
+    }
+
+    static class FluentHeroFactory
+    {
+        public static IHeroFactory Init()
+        {
+            return new HeroFactory(new Hero());
+        }
+    }
+}
